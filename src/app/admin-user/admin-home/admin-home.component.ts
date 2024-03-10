8496f9fa1713d5e1ba5740705e14b384f4c15394
Nimra <92775652+NimraYounus@users.noreply.github.com>
2024-03-10 22:35:47 +0500

    add crad on admin-home

--- src/app/admin-user/admin-home/admin-home.component.ts
+++ src/app/admin-user/admin-home/admin-home.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-home',
   templateUrl: './admin-home.component.html',
-  styleUrls: ['./admin-home.component.css']
+  styleUrls: ['./admin-home.component.css'],
 })
 export class AdminHomeComponent {
-
+  cardList = [
+    {
+      icon: 'people',
+      name: 'TOTAL MEMBERS',
+      quantity: '105',
+    },
+    {
+      icon: 'library_add_check',
+      name: 'ISSUED BOOKS',
+      quantity: '30',
+    },
+    {
+      icon: 'keyboard_return',
+      name: 'NOT RETURNED',
+      quantity: '',
+    },
+    {
+      icon: 'Online Prediction',
+      name: 'LOGGED IN USERS',
+      quantity: '5',
+    },
+  ];
 }
