84baf992cd07ded1be160081698f996c73d9a6aa
Nimra <92775652+NimraYounus@users.noreply.github.com>
2024-03-18 02:24:14 +0500

    books screen

--- src/app/admin-user/admin-sidebar/sideBar-item/books/books.component.ts
+++ src/app/admin-user/admin-sidebar/sideBar-item/books/books.component.ts
@@ -3,8 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.css']
+  styleUrls: ['./books.component.css'],
 })
 export class BooksComponent {
-
+  BooksList = [
+    {
+      memberId: '#48964',
+      memberName: 'Karla Morrison',
+      bookName: 'Mangolia Palace',
+      bookAuther: 'Cristofer Bator',
+      borrowedDate:'17/3/2023',
+      returnDate:'12/3/2023',
+      status: 'Available',
+    },
+  ];
 }
