469671e10b57457ad62bb77e04fd5c7a9eff058c
Nimra <92775652+NimraYounus@users.noreply.github.com>
2024-03-18 00:42:24 +0500

    completed members sceen

--- src/app/admin-user/admin-sidebar/sideBar-item/members/members.component.ts
+++ src/app/admin-user/admin-sidebar/sideBar-item/members/members.component.ts
@@ -3,8 +3,94 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-members',
   templateUrl: './members.component.html',
-  styleUrls: ['./members.component.css']
+  styleUrls: ['./members.component.css'],
 })
 export class MembersComponent {
+  overDuesPeople = [
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Karla Morrison',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '50',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Mamie Rodgers',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '150',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Terence Reese',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '100',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Karla Morrison',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '50',
+    },
 
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Diana Curry',
+      email: 'email123@gmail.com',
+      status: 'DEACTIVE',
+      duedate: '17/3/2024',
+      fine: '200',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Gilbert Santos',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '100',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Karla Morrison',
+      email: 'email123@gmail.com',
+      status: 'ACTIVE',
+      duedate: '17/3/2024',
+      fine: '50',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Mamie Rodgers',
+      email: 'email123@gmail.com',
+      status: '3',
+      duedate: '17/3/2024',
+      fine: '150',
+    },
+    {
+      memberId: '#48964',
+      registerid: '3012',
+      memberName: 'Terence Reese',
+      email: 'email123@gmail.com',
+      status: 'DEACTIVE',
+      duedate: '17/3/2024',
+      fine: '100',
+    },
+  ];
+  search() {
+    //
+  }
 }
