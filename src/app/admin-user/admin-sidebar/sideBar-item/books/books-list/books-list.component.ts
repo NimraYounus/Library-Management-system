246b6927d9ad5b8c1d413613e3ed10c38ec4a39c
Nimra <92775652+NimraYounus@users.noreply.github.com>
2024-03-24 03:37:39 +0500

    book-list

--- src/app/admin-user/admin-sidebar/sideBar-item/books/books-list/books-list.component.ts
+++ src/app/admin-user/admin-sidebar/sideBar-item/books/books-list/books-list.component.ts
@@ -3,8 +3,39 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-books-list',
   templateUrl: './books-list.component.html',
-  styleUrls: ['./books-list.component.css']
+  styleUrls: ['./books-list.component.css'],
 })
 export class BooksListComponent {
-
+  booksCategories = [
+    {
+      booksCategories: 'English Novels',
+      books: [
+        {
+          bookName: '',
+          author: '',
+          discription: '',
+          barCode: '',
+          yearOfPublish: '',
+          reviews: '',
+          bookImge: '',
+          bookStatus: '',
+        },
+      ],
+    },
+    {
+      booksCategories: 'English Novels',
+      books: [
+        {
+          bookName: '',
+          author: '',
+          discription: '',
+          barCode: '',
+          yearOfPublish: '',
+          reviews: '',
+          bookImge: '',
+          bookStatus: '',
+        },
+      ],
+    },
+  ];
 }
